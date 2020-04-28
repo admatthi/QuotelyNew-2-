75473aef78afd548dfe2bf19e43943760ca62c1d
Tushali <53653089+tushali024@users.noreply.github.com>
2020-04-28 20:59:39 +0530

    Solved notification issue

--- Firebase_Cloud_Functions/functions/index.js
+++ Firebase_Cloud_Functions/functions/index.js
@@ -3,105 +3,108 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 var db = admin.database();
 
+// exports.devNotficationCheck = functions.pubsub.schedule('*/5 * * * *')
+//   .timeZone('Asia/Calcutta') // Users can choose timezone - default is America/Los_Angeles
+//   .onRun(async context => {
+
+//     console.log('This will be run every 5 minute CST!');
+//     // birthdayNotificationCheck("morning");
+//     await sendHeadlineNotification();
+//     return null;
+//   });
+
 exports.firstNotficationCheck = functions.pubsub.schedule('0 8 * * *')
   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
-
+  .onRun(async context => {
     console.log('This will be run every day at 8:00 AM CST!');
     // birthdayNotificationCheck("morning");
-    sendHeadlineNotification();
+    await sendHeadlineNotification();
     return null;
-});
+  });
 
 exports.secondNotficationCheck = functions.pubsub.schedule('0 9 * * *')
   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
-
+  .onRun(async context => {
     console.log('This will be run every day at 9:00 AM CST!');
     // birthdayNotificationCheck("morning");
-    sendHeadlineNotification();
+    await sendHeadlineNotification();
     return null;
-});
+  });
 
 exports.thirdNotficationCheck = functions.pubsub.schedule('0 10 * * *')
   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
-
-    console.log('This will be run every day at 10:00 AM CST!');
+  .onRun(async context => {
+    console.log('This will be run every day at 9:00 AM CST!');
     // birthdayNotificationCheck("morning");
-    sendHeadlineNotification();
+    await sendHeadlineNotification();
     return null;
-});
+  });
 
 exports.forthNotficationCheck = functions.pubsub.schedule('0 11 * * *')
   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
-
+  .onRun(async context => {
     console.log('This will be run every day at 11:00 AM CST!');
     // birthdayNotificationCheck("morning");
-    sendHeadlineNotification();
+    await sendHeadlineNotification();
     return null;
-});
+  });
 
 exports.fifthNotficationCheck = functions.pubsub.schedule('0 12 * * *')
   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
-
+  .onRun(async context => {
     console.log('This will be run every day at 12:00 AM CST!');
     // birthdayNotificationCheck("morning");
-    sendHeadlineNotification();
+    await sendHeadlineNotification();
     return null;
-});
+  });
 
 exports.sixthNotficationCheck = functions.pubsub.schedule('0 13 * * *')
   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
+  .onRun(async context => {
 
     console.log('This will be run every day at 13:00 AM CST!');
     // birthdayNotificationCheck("morning");
-    sendHeadlineNotification();
+    await sendHeadlineNotification();
     return null;
-});
+  });
 
 exports.seventhNotficationCheck = functions.pubsub.schedule('0 14 * * *')
   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
+  .onRun(async context => {
 
     console.log('This will be run every day at 14:00  CST!');
     // birthdayNotificationCheck("morning");
-    sendHeadlineNotification();
+    await sendHeadlineNotification();
     return null;
-});
+  });
 
 exports.eighthNotficationCheck = functions.pubsub.schedule('0 15 * * *')
   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
+  .onRun(async context => {
 
     console.log('This will be run every day at 15:00 CST!');
     // birthdayNotificationCheck("morning");
-    sendHeadlineNotification();
+    await sendHeadlineNotification();
     return null;
-});
+  });
 
 exports.ninethNotficationCheck = functions.pubsub.schedule('0 16 * * *')
   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
-
+  .onRun(async context => {
     console.log('This will be run every day at 16:00 CST!');
     // birthdayNotificationCheck("morning");
-    sendHeadlineNotification();
+    await sendHeadlineNotification();
     return null;
-});
+  });
 
 exports.tenthNotficationCheck = functions.pubsub.schedule('0 17 * * *')
   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
-  .onRun((context) => {
-
+  .onRun(async context => {
     console.log('This will be run every day at 17:00 CST!');
     // birthdayNotificationCheck("morning");
-    sendHeadlineNotification();
+    await sendHeadlineNotification();
     return null;
-});
+  });
 
 // exports.testNotificationCheck = functions.pubsub.schedule('every 2 minutes')
 //   .timeZone('America/Costa_Rica') // Users can choose timezone - default is America/Los_Angeles
@@ -113,75 +116,90 @@ exports.tenthNotficationCheck = functions.pubsub.schedule('0 17 * * *')
 //     return null;
 // });
 
-function sendHeadlineNotification() {
+async function sendHeadlineNotification() {
 
-    var booksRef = db.ref("/AllBooks1/Relationships")
-    return booksRef.once("value", function(snapshot) {
+  var booksRef = db.ref("/AllBooks1/Relationships")
 
-        var books = snapshot.val();
-        for (var bookId in books) {
-            console.log("book ", books[bookId]);
-            var book = books[bookId];
-            if (book['isSent'] == 'true') {
-                continue;
-            }
+  return await booksRef.once("value", async function (snapshot) {
 
-            var headerline = book["Headline1"];
-            if(headerline !== null && headerline !== '') {
-                sendToDevices(headerline)
-             }
+    var books = snapshot.val();
 
-            booksRef.child(bookId).update({
-                isSent: 'true',
-            });
-            break;
-        }
+    console.log("books ", books);
 
-    });
-    
+    for (var bookId in books) {
+
+      var book = books[bookId];
+
+      if (book['isSent'] == 'true') {
+        continue;
+      }
+
+      var headerline = book["Headline1"];
+
+      if (headerline !== null && headerline !== '') {
+        await sendToDevices(headerline)
+      }
+
+      booksRef.child(bookId).update({
+        isSent: 'true',
+      });
+      break;
+    }
+
+  });
+
 }
 
-function sendToDevices(headerline) {
-    var pushTokensRef = db.ref("/pushTokens");
-    return pushTokensRef.once("value", function (snapshot) {
-        var pushTokens = snapshot.val();
-        for (var token in pushTokens) {
-            console.log("headline ", headerline);
-            console.log("push token ", pushTokens[token]);
-            sendPushNotification(pushTokens[token], '', headerline, 0);
-        }
-    });
+async function sendToDevices(headerline) {
+  var pushTokensRef = db.ref("/pushTokens");
+  return await pushTokensRef.once("value", async function (snapshot) {
+    var pushTokens = snapshot.val();
+    //console.log("Push Token ", pushTokens);
+    const pushTokenList = [];
+    for (var token in pushTokens) {
+      console.log("headline ", headerline);
+      console.log("push token ", pushTokens[token]);
+      var token = pushTokens[token]
+      pushTokenList.push(token);
+    }
+    console.log("Push Token ", pushTokenList);
+    if (pushTokenList.length > 0) {
+      sendPushNotification(pushTokenList, ' ', headerline, 0);
+    }
+  });
 }
 
 function sendPushNotification(token, title, text, badge) {
 
-    // console.log("from ID ", fromUserID, " fromUsername ", fromUsername);
+  // console.log("from ID ", fromUserID, " fromUsername ", fromUsername);
 
-    let payload = {
-        notification: {
-            title: title,
-            body: text,
-            sound: 'default',
-            badge: badge.toString(),
-            ios_badgeType: 'Increase',
-            ios_badgeCount: '1',
-            click_action: 'ACTIONABLE'
-        },
-        data: {
-        }
-    };
+  let payload = {
+    notification: {
+      title: title,
+      body: text,
+      sound: 'default',
+      badge: badge.toString(),
+      ios_badgeType: 'Increase',
+      ios_badgeCount: '1',
+      click_action: 'ACTIONABLE'
+    },
+    data: {
+      text: text
+    }
+  };
 
-    // console.log('push notification sent ', token);
+  console.log('TOKEN', token);
+  console.log('push notification sent ', payload);
 
-    admin.messaging().sendToDevice([token], payload)
+  admin.messaging().sendToDevice(token, payload)
     .then((response) => {
-            console.log('Successfully sent message:', response);
-            // console.log('error message ', response['results']);
+      console.log('Successfully sent message:', response);
+      // console.log('error message ', response['results']);
     }).catch((error) => {
-            console.log('Error sending message:', error);
+      console.log('Error sending message:', error);
     });
-            
-    // return admin.messaging().sendToDevice(token, payload);
+
+  // return admin.messaging().sendToDevice(token, payload);
 }
 
 
